1941ebfab64bf976990a571e4cae9eb14069cde3
tanjunwei03 <tanjunwei03@gmail.com>
2021-06-01 00:09:04 +0800

    Copying ariana's website

---
+++ README.md
@@ -0,0 +1,1 @@
+# Some gh page
---
+++ static/mathjax_macros.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+tex: {
+macros: {
+mb: ["{\\mathbb #1}", 1],
+mc: ["{\\mathcal #1}", 1],
+mf: ["{\\mathfrak #1}", 1],
+Aut: ["{\\operatorname{Aut}\\left(#1\\right)}", 1],
+Gal: ["{\\operatorname{Gal}\\left(#1\\right)}", 1],
+PSL: ["{\\operatorname{PSL}_{#1}\\left(#2\\right)}", 2, '']
+}
+}
+};
